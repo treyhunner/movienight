9f376248ce4a8286c5470e9da06e3e70c4fd2c50
Trey Hunner <trey@treyhunner.com>
2013-04-27 21:17:26 -0700

    Change title of website to "Movie Night"

--- movienight/admin.py
+++ movienight/admin.py
@@ -3,7 +3,7 @@ from flask.ext.admin.contrib.sqlamodel import ModelView
 
 from movienight import db, app, models
 
-admin = Admin(app)
+admin = Admin(app, name="Movie Night")
 
 admin.add_view(ModelView(models.Person, db.session, endpoint='person'))
 admin.add_view(ModelView(models.Event, db.session, endpoint='event'))
