c83b7684b7d930b0525506471007553a42aae8cd
Trey Hunner <trey@treyhunner.com>
2013-04-28 22:47:50 -0700

    Set SECRET_KEY from env

--- movienight/config.py
+++ movienight/config.py
@@ -3,6 +3,7 @@ import os
 base = os.path.abspath(os.path.dirname(__file__))
 
 default_database_url = "sqlite:///{0}/movienight.db".format(base)
+default_secret = "\xccK\xd6V-L\xe1\xeeR:\x84\x05y\xcb)\xb0\x9c\x86\x9bWRE%\xf4"
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default_database_url)
-SECRET_KEY = "\xccK\xd6V-L\xe1\xeeR:\x84\x05y\xcb)\xb0\x9c\x86\x9bWRE%\xf4"
+SECRET_KEY = os.environ.get('SECRET_KEY', default_secret)
