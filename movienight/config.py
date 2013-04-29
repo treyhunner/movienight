cf8e6c5cb7eb0017e137da125ac2650ba1495ccb
Trey Hunner <trey@treyhunner.com>
2013-04-28 19:11:45 -0700

    Set DATABSE_URL from env

--- movienight/config.py
+++ movienight/config.py
@@ -2,5 +2,7 @@ import os
 
 base = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///{0}/movienight.db".format(base)
+default_database_url = "sqlite:///{0}/movienight.db".format(base)
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default_database_url)
 SECRET_KEY = "\xccK\xd6V-L\xe1\xeeR:\x84\x05y\xcb)\xb0\x9c\x86\x9bWRE%\xf4"
